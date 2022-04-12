07107969446c5e4d80af49bff86e386e3cf88623
Leonid <simplesession@gmail.com>
2022-04-12 12:04:55 +0500

    add ajax totals, @login_required

--- basket/views.py
+++ basket/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
-
-# Create your views here.
+from django.template.loader import render_to_string
 from basket.models import Basket
 from mainapp.models import Product
 
 
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
+
+@login_required
 def basket_add(request, id):
     user_select = request.user
     product = Product.objects.get(id=id)
@@ -16,8 +21,30 @@ def basket_add(request, id):
         basket.save()
     else:
         Basket.objects.create(user=user_select, product=product, quantity=1)
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    products = Product.objects.all()
+    context = {'products': products}
+    result = render_to_string('mainapp/includes/card.html', context)
+    return JsonResponse({'result': result})
 
+
+@login_required
 def basket_remove(request, basket_id):
     Basket.objects.get(id=basket_id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request,basket_id,quantity):
+    if is_ajax(request=request):
+        basket = Basket.objects.get(id=basket_id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+
+        baskets = Basket.objects.filter(user = request.user)
+        context = {'baskets':baskets}
+
+        result = render_to_string('basket/basket.html',context)
+        return JsonResponse({'result':result})
